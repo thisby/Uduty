ccf9ccfe32c4f1e2f41ca939e8695dd47b00d18b
thisby <bentaleb.youness@gmail.com>
2017-05-13 00:06:08 +0200

    Remove bad package from composer json

--- config/cli-config.php
+++ config/cli-config.php
@@ -2,6 +2,7 @@
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
 // replace with file to your own project bootstrap
+echo getcwd();
 require_once __DIR__.'/../bootstrap.php';
 
 // replace with mechanism to retrieve EntityManager in your app
